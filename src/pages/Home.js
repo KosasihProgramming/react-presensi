e8a720d83e2fba6eaad9fec330ebd99357fea1d7
komangchandra <komangchandraaa1@gmail.com>
2024-03-29 11:17:46 +0700

    feat: membuat project presensi

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Navigation from "../components/Navigation";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
