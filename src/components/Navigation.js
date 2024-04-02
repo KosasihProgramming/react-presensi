39cddcac4a88c7263bc31b0d560fba462a06ca74
komangchandra <komangchandraaa1@gmail.com>
2024-04-02 11:24:30 +0700

    fix: navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,6 +6,7 @@ import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 const navList = [
   { name: "Dashboard", href: "/", current: true },
   { name: "Shift", href: "/shift", current: false },
+  { name: "Jadwal", href: "/jadwal", current: false },
   { name: "Kehadiran", href: "/kehadiran", current: false },
   { name: "Absen", href: "/presensi", current: false },
 ];
