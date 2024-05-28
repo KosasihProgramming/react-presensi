88cd0679e1e27cac449e06d94e3323edc6edc9cf
komangchandra <komangchandraaa1@gmail.com>
2024-05-28 10:25:43 +0700

    fix: urlAPI

--- src/config/global.js
+++ src/config/global.js
@@ -1,4 +1,4 @@
-export const urlAPI = "http://localhost:5000";
+export const urlAPI = "https://komangchandra.my.id/presensi-test/";
 // export const urlAPI = "http://26.29.249.145:2500";
 export const botTokenTelegram =
   "6450512353:AAEPzql6mikTouuEse_S_GhzA7XlmC1lNgE";
