d2ae99cc508fa5c7f2f0d0deb8e9b2b51f0e88db
komangchandra <komangchandraaa1@gmail.com>
2024-05-29 13:21:10 +0700

    fix: urlAPI

--- src/config/global.js
+++ src/config/global.js
@@ -1,4 +1,7 @@
-export const urlAPI = "http://202.157.189.177:5000";
+const protocol = window.location.protocol; // Akan menjadi 'http:' atau 'https:'
+const host = "202.157.189.177:5000"; // Host API Anda
+export const urlAPI = `${protocol}//${host}`;
+
 // export const urlAPI = "http://26.29.249.145:2500";
 export const botTokenTelegram =
   "6450512353:AAEPzql6mikTouuEse_S_GhzA7XlmC1lNgE";
